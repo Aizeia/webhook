c95400a9f58fdc3aa37558f64a78a6c4e482b7ca
Aizeia <bobedibabedi@gmail.com>
2026-02-26 12:22:26 +0100

    gg
    sdf

--- index.js
+++ index.js
@@ -1,32 +1,27 @@
-const express = require("express");
-const app = express();
-
-app.use(express.json());
-
 app.post("/webhook", (req, res) => {
-  console.log("Incoming request:", JSON.stringify(req.body, null, 2));
 
   const intentName = req.body.queryResult.intent.displayName;
-  let responseText = "Webhook working!";
+  let responseText = "Something went wrong.";
 
   if (intentName === "PRIO.Create_Support_Ticket") {
-    responseText = "Support ticket created successfully.";
+    responseText = "Do you want to confirm the ticket?";
   }
 
-  if (intentName === "Ticket.Check_Status") {
-    responseText = "Your ticket is currently being processed.";
+  else if (intentName === "Ticket.Confirm_Details") {
+
+    const ticketId = Math.floor(Math.random() * 10000);
+
+    responseText = `Perfect. I am submitting the ticket now… Ticket ID: ${ticketId}`;
+  }
+
+  else if (intentName === "Ticket.Check_Status") {
+
+    const ticketId = req.body.queryResult.parameters.ticket_id;
+
+    responseText = `Ticket ${ticketId} is currently being processed.`;
   }
 
   res.json({
     fulfillmentText: responseText
   });
-});
-
-app.get("/", (req, res) => {
-  res.send("Server is running");
-});
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
 });
